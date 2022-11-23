94005db92b43d02327f6473f5b5a363202fd20be
Loïc Faure-Lacroix <lamerstar@gmail.com>
2022-11-23 15:31:50 -0500

    Allow import faking controllers/route

--- odoo_tools_openapi/odoo.py
+++ odoo_tools_openapi/odoo.py
@@ -1,1 +1,10 @@
-from odoo.http import request, route, Controller
+try:
+    from odoo.http import request, route, Controller
+except ImportError:
+    request = None
+    Controller = type('Controller', (object,), {})
+
+    def route(*args, **kwargs):
+        def wrap(func):
+            return func
+        return wrap
