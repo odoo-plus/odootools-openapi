911a84d373166406877a5e0b9644727c9451fa85
Loïc Faure-Lacroix <lamerstar@gmail.com>
2022-11-25 17:05:41 -0500

    Added plainjson request

---
+++ odoo_tools_openapi/_odoo/common/odoo/http.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import werkzeug
+
+from odoo_tools_openapi.odoo.http import Response, JsonRequest
+from odoo_tools_openapi.odoo.tools import date_utils
+
+
+_logger = logging.getLogger(__name__)
+
+
+class PlainJsonRequest(JsonRequest):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.params = {}
+
+        args = self.httprequest.args
+        request = None
+
+        # regular jsonrpc2
+        request = self.httprequest.get_data().decode(self.httprequest.charset)
+
+        # Read POST content or POST Form Data named "request"
+        try:
+            self.jsonrequest = json.loads(request)
+        except ValueError:
+            msg = 'Invalid JSON data: %r' % (request,)
+            _logger.info('%s: %s', self.httprequest.path, msg)
+            raise werkzeug.exceptions.BadRequest(msg)
+
+    def _json_response(self, result=None, error=None):
+        mime = 'application/json'
+        body = json.dumps(result, default=date_utils.json_default)
+
+        return Response(
+            body, status=error and error.pop('http_status', 200) or 200,
+            headers=[('Content-Type', mime), ('Content-Length', len(body))]
+        )
